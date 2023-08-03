77d14c6da1ee74ec2821dce117e41a458b2b5c83
heyihan-ola <he.yihan@olaola.chat>
2023-08-03 20:09:54 +0800

    fix gen_db

--- gen_db.sh
+++ gen_db.sh
@@ -11,9 +11,10 @@ fi
 export GF_GCFG_FILE=config/config_cli.toml
 
 genproto() {
-    gf-cli gen pbentity -g $2 -t $1 -path gen_proto/db/$2
+    gf-cli gen pbentity -g $2 -t $1 -path gen_proto/db/$2 -option "option go_package = \"./gen_pb/db/$2\";"
 
     if [ $? -ne 0 ]; then
+        echo "genproto failed"
         exit 1
     fi
 }
@@ -26,6 +27,23 @@ gendao(){
     fi
 
     if [ $? -ne 0 ]; then
+        echo "gendao failed"
+        exit 1
+    fi
+}
+
+gengo() {
+    protoc --go_out=. --proto_path=gen_proto/db/$2 entity_$1.proto
+    if [ $? -ne 0 ]; then
+        echo "gengo failed"
+        exit 1
+    fi
+}
+
+inject() {
+    protoc-go-inject-tag -input=gen_pb/db/$2/entity_$1.pb.go
+    if [ $? -ne 0 ]; then
+        echo "inject failed"
         exit 1
     fi
 }
@@ -35,6 +53,8 @@ sed "s/dbname/$1/g" config/modelindex.tpl > modelindex.tpl
 
 genproto $2 $1
 gendao $2 $1
+gengo $2 $1
+inject $2 $1
 
 rm daoindex.tpl modelindex.tpl
 
