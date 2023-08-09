869d15c632816a6a71ed4903b8def2f5b08ae6d2
hubicheng-ola <Hu.Bicheng@olaola.chat>
2023-08-09 17:12:39 +0800

    rebase

--- scripts/gen_rpc.sh
+++ scripts/gen_rpc.sh
@@ -9,6 +9,10 @@ export PROTO_SERV="protoc --rbp-rpc_out=${GOPATH}/src -I=./protoc-gen-rbp-rpc/pr
 
 ${PROTOC} rpc/user/user_profile_message.proto
 ${PROTO_SERV} rpc/user/user_profile_service.proto
+${PROTOC} rpc/voice_lover/voice_lover_message.proto
+${PROTO_SERV} rpc/voice_lover/voice_lover_main_service.proto
+${PROTOC} rpc/voice_lover/voice_lover_common.proto
+${PROTO_SERV} rpc/voice_lover/voice_lover_admin_service.proto
 
 
 
