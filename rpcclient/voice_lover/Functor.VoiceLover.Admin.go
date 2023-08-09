59b9e44e2be620efce2314bcf04319d9b78710e0
hubicheng-ola <Hu.Bicheng@olaola.chat>
2023-08-09 15:20:43 +0800

    fix

--- rpcclient/voice_lover/Functor.VoiceLover.Admin.go
+++ rpcclient/voice_lover/Functor.VoiceLover.Admin.go
@@ -1,7 +1,11 @@
 package voice_lover
 
 import (
+	"context"
+
 	"github.com/olaola-chat/rbp-proto/rpcclient/base"
+
+	"github.com/olaola-chat/rbp-proto/gen_pb/rpc/voice_lover"
 )
 
 // VoiceLoverAdmin *voiceLoverAdmin
@@ -13,4 +17,16 @@ var VoiceLoverAdmin = &voiceLoverAdmin{
 
 type voiceLoverAdmin struct {
 	*base.Base
+}
+
+func (s *voiceLoverAdmin) GetAudioEdit(ctx context.Context, req *voice_lover.ReqGetAudioEdit) (*voice_lover.ResGetAudioEdit, error) {
+	reply := &voice_lover.ResGetAudioEdit{}
+	err := s.Call(ctx, "GetAudioEdit", req, reply)
+	return reply, err
+}
+
+func (s *voiceLoverAdmin) GetAudioDetail(ctx context.Context, req *voice_lover.ReqGetAudioDetail) (*voice_lover.ResGetAudioDetail, error) {
+	reply := &voice_lover.ResGetAudioDetail{}
+	err := s.Call(ctx, "GetAudioDetail", req, reply)
+	return reply, err
 }
