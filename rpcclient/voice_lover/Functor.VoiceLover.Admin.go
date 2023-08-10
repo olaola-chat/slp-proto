cb0b08e70e5791a6f6fbe8d2fd761460c1b11481
hubicheng-ola <Hu.Bicheng@olaola.chat>
2023-08-10 16:49:23 +0800

    reason

--- rpcclient/voice_lover/Functor.VoiceLover.Admin.go
+++ rpcclient/voice_lover/Functor.VoiceLover.Admin.go
@@ -36,3 +36,9 @@ func (s *voiceLoverAdmin) AuditAudio(ctx context.Context, req *voice_lover.ReqAu
 	err := s.Call(ctx, "AuditAudio", req, reply)
 	return reply, err
 }
+
+func (s *voiceLoverAdmin) GetAuditAudioReason(ctx context.Context, req *voice_lover.ReqGetAuditAudioReason) (*voice_lover.ResGetAuditAudioReason, error) {
+	reply := &voice_lover.ResGetAuditAudioReason{}
+	err := s.Call(ctx, "GetAuditAudioReason", req, reply)
+	return reply, err
+}
