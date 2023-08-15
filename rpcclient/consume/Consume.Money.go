9a0e10acc20048daf93a80edfc21a004c2b08e46
liuwei-ola <liu.wei@olaola.chat>
2023-08-15 12:14:46 +0800

    update

--- rpcclient/consume/Consume.Money.go
+++ rpcclient/consume/Consume.Money.go
@@ -8,24 +8,24 @@ import (
 	"github.com/olaola-chat/rbp-proto/gen_pb/rpc/consume"
 )
 
-// UserProfile *userProfile
-var UserProfile = &userProfile{
+// ConsumeMoney *consumeMoney
+var ConsumeMoney = &consumeMoney{
 	&base.Base{
 		Name: "Consume.Money",
 	},
 }
 
-type userProfile struct {
+type consumeMoney struct {
 	*base.Base
 }
 
-func (s *userProfile) GetRequestId(ctx context.Context, req *consume.GetRequestIdReq) (*consume.GetRequestIdRes, error) {
+func (s *consumeMoney) GetRequestId(ctx context.Context, req *consume.GetRequestIdReq) (*consume.GetRequestIdRes, error) {
 	reply := &consume.GetRequestIdRes{}
 	err := s.Call(ctx, "GetRequestId", req, reply)
 	return reply, err
 }
 
-func (s *userProfile) CommoditySend(ctx context.Context, req *consume.CommoditySendReq) (*consume.CommoditySendRes, error) {
+func (s *consumeMoney) CommoditySend(ctx context.Context, req *consume.CommoditySendReq) (*consume.CommoditySendRes, error) {
 	reply := &consume.CommoditySendRes{}
 	err := s.Call(ctx, "CommoditySend", req, reply)
 	return reply, err
