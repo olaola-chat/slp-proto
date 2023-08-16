1af450d60c9b74f8c2c1f9d084b444bdef414ff0
liuwei-ola <liu.wei@olaola.chat>
2023-08-16 11:39:20 +0800

    add collect

--- rpcclient/voice_lover/Functor.VoiceLover.Main.go
+++ rpcclient/voice_lover/Functor.VoiceLover.Main.go
@@ -79,6 +79,12 @@ func (s *voiceLoverMain) IsUserCollectAlbum(ctx context.Context, req *voice_love
 	return reply, err
 }
 
+func (s *voiceLoverMain) Collect(ctx context.Context, req *voice_lover.ReqCollect) (*voice_lover.ResCollect, error) {
+	reply := &voice_lover.ResCollect{}
+	err := s.Call(ctx, "Collect", req, reply)
+	return reply, err
+}
+
 func (s *voiceLoverMain) SubmitAudioComment(ctx context.Context, req *voice_lover.ReqAudioSubmitComment) (*voice_lover.ResCommonPost, error) {
 	reply := &voice_lover.ResCommonPost{}
 	err := s.Call(ctx, "SubmitAudioComment", req, reply)
